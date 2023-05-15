acd4002c446e56eb500c33ac87ba2e1908c7ee0e
MilenaKozima <mkozima06@gmail.com>
2023-05-15 06:40:33 -0300

    Versão 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import palavras from "./palavras";
+import forca0 from "./assets/forca0.png"
+import forca1 from "./assets/forca1.png"
 
 export default  function App() {
   return (
     <div className="Jogo">
-      <mg className="boneco" />
+      <img className="boneco b0" src={forca0}/>
+      <img className="boneco b1 escondida" src={forca1}/>
+      <div className="botIniciar">
+        <button>Iniciar o jogo</button>
+      </div>
     </div>
   );
 }
